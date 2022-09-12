c182677ff3795973e21851958a402cf97f6faff7
shafayet035 <shafayet035@gmail.com>
2022-09-12 19:59:32 +0600

    dist deleted

--- dist/types/AccordionStyle.d.ts
+++
@@ -1,7 +0,0 @@
-export declare const AccordionItemStyle: import("styled-components").StyledComponent<"div", any, {}, never>;
-export declare const AccordionItemHeader: import("styled-components").StyledComponent<"div", any, {}, never>;
-export declare const AccordionItemHeaderTitle: import("styled-components").StyledComponent<"h3", any, {}, never>;
-export declare const AccordionItemIcon: import("styled-components").StyledComponent<"div", any, {
-    open: boolean;
-}, never>;
-export declare const AccordionItemBody: import("styled-components").StyledComponent<"div", any, {}, never>;
